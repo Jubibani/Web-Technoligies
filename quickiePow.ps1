9005a076f9a308c140fa1e991d69fe2853d16c4c
Jubibani <strawberryloli3@gmail.com>
2024-02-28 23:27:31 +0800

    Importing Selenium Goes Wrong
    I was debugging and solidifying the script automation. I also did output_debugged the counter and turned it to a for loop instead of a static output print.
    
    Moved on to importing the selenium module. 80% of my time was contributed finding out the problem of importing the selenium. After running out of time. I figured out i was importing the Selenium the wrong way. Next time, I have to Import it properly.

--- quickiePow.ps1
+++ quickiePow.ps1
@@ -1,14 +1,11 @@
 ﻿
-#importing selenium
-Import-Module Selenium
+#importing selenium with where you installed your selenium module
+Import-Module "C:\Program Files\Python311\Lib\site-packages\selenium"
 #quickiePow is pwsh version for Quicki.bat file
 Write-Host "---Hello we opened quickiePow!---"
 
-#password setting
+#password setting for the script
 $password = 'HH211R'
-
-#starting a web session
-$driver = Start-SeChrome
 
 #function to be executed
 function openWorkspace() {
@@ -18,12 +15,18 @@ function openWorkspace() {
     # Navigate to the website where you want to log in
     $driver.Navigate().GoToUrl("https://www.notion.so/Student-Notes-d19082c1bdf942ebaef6678b0ab342b2")
 
-    # Find the username/email and password input fields and enter credentials
-    $usernameField = $driver.FindElementById("input")
-    $passwordField = $driver.FindElementById("password")
-    $emailNotion.SendKeys("strawberryloli3@gmail.com")
-    $passwordField.SendKeys("your_password")
-    Start-Process -FilePath "C:\Program Files\Google\Chrome\Application\chrome.exe" -ArgumentList "--incognito", "--new-window https://www.notion.so/Student-Notes-d19082c1bdf942ebaef6678b0ab342b2" #"add here a VPN chatGPT"
+    # Find the elennts to insert the credential
+    $emailField = $driver.FindElementById("notion-email-input-2")
+
+    #wait 3 seconds before inputting passowrd
+    $secondsToSleep = 3
+
+    for($countStartsFrom = $secondsToSleep; $countStartsFrom -gt 0; $countStartsFrom--){
+    Write-Host "waiting in $countStartsFrom"
+    Start-Sleep -Seconds 1
+    }
+
+    $emailField.SendKeys("cao5224@sdtudents.uc-bcf.edu.ph")
 }
 
 
