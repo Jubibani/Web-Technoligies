530dd9db92c40487fcb2f582f6707e2067ff80ad
Jubibani <strawberryloli3@gmail.com>
2024-03-04 18:39:24 +0800

    Remodifying Program Structure

--- quickiePow.ps1
+++ quickiePow.ps1
@@ -1,29 +1,34 @@
-﻿# Set the path to Selenium WebDriver DLL and ChromeDriver
-$workingPath = "C:\selenium-selenium-4.18.0"
-$chromeDriverPath = Join-Path -Path $workingPath -ChildPath "chromedriver.exe"
-$webDriverDllPath = Join-Path -Path $workingPath -ChildPath "WebDriver.dll"
+﻿# Import Selenium WebDriver module
+Import-Module Selenium
 
-# Add the path to Selenium WebDriver DLL to the assembly
-Add-Type -Path $webDriverDllPath
+# Set the path to ChromeDriver
+$chromeDriverPath = "C:\selenium-selenium-4.18.0\chromedriver.exe"
 
-# Start Chrome WebDriver
-$driver = [OpenQA.Selenium.Chrome.ChromeDriver]::new($chromeDriverPath)
+# Create a new ChromeDriver instance
+$driver = New-Object OpenQA.Selenium.Chrome.ChromeDriver($chromeDriverPath)
+function loginToQuickie () {
+    $passwordToQuickie = "HH211R"
+    $passwordInput = Read-Host "Enter Passowrd" -AsSecureString
+    if ($passwordInput == $passwordToQuickie) {
+        Read-Host "Access Granted!"
+        # Navigate to the website where you want to log in
+        $driver.Navigate().GoToUrl("https://www.notion.so/Student-Notes-d19082c1bdf942ebaef6678b0ab342b2")
 
-# Navigate to the website where you want to log in
-$driver.Navigate().GoToUrl("https://www.notion.so/Student-Notes-d19082c1bdf942ebaef6678b0ab342b2")
+        # Find the element to insert the email
+        $emailField = $driver.FindElementById("notion-email-input-2")
+        $emailField.SendKeys("cao5224@sdtudents.uc-bcf.edu.ph")
 
-# Find the element to insert the email
-$emailField = $driver.FindElementById("notion-email-input-2")
-$emailField.SendKeys("cao5224@sdtudents.uc-bcf.edu.ph")
+        # Prompt for password
+        $passwordInput = Read-Host "Password: " -AsSecureString
+        $passwordPlainText = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwordInput))
 
-# Prompt for password
-$passwordInput = Read-Host "Password: " -AsSecureString
-$passwordPlainText = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwordInput))
+        # Example: Finding and interacting with password field
+        $passwordField = $driver.FindElementById("password-field-id")
+        $passwordField.SendKeys($passwordPlainText)
 
-# Example: Finding and interacting with password field
-$passwordField = $driver.FindElementById("password-field-id")
-$passwordField.SendKeys($passwordPlainText)
+        # Example: Clicking on login button
+        $loginButton = $driver.FindElementById("login-button-id")
+        $loginButton.Click()
 
-# Example: Clicking on login button
-$loginButton = $driver.FindElementById("login-button-id")
-$loginButton.Click()
+    }
+}
