0cb6ad9ef6d8ec031fd044316ee85e210dbe4b0d
Jubibani <strawberryloli3@gmail.com>
2024-03-05 18:36:18 -0800

    Couldn't sign in Security Error
    Navigating to the elements and automating Actions for loggin in the site seems okay. however, due to security. automations does not seem possible to other sites. currently finding a way to by pass this problem
    
    more to be followed

--- quickiePow.ps1
+++ quickiePow.ps1
@@ -18,24 +18,50 @@ function loginToQuickie {
     $driver.Navigate().GoToUrl("https://www.notion.so/Student-Notes-d19082c1bdf942ebaef6678b0ab342b2")
 
     
-    # Wait for the email input field to be visible
-    $wait = New-Object OpenQA.Selenium.Support.UI.WebDriverWait($driver, [timespan]::FromSeconds(10))
-    $emailField = $wait.Until({
-        param ($driver)
-        $element = $driver.FindElementById("notion-email-input-2")
-        if ($element.Displayed) {
-            $element
-        }
-    })
-    #im adding a delay
-    Write-Host "delaying"
-    Start-Sleep -Seconds 5
+    # # Wait for the email input field to be visible
+    # $wait = New-Object OpenQA.Selenium.Support.UI.WebDriverWait($driver, [timespan]::FromSeconds(10))
+    # $emailField = $wait.Until({
+    #     param ($driver)
+    #     $element = $driver.FindElementById("notion-email-input-2")
+    #     if ($element.Displayed) {
+    #         $element
+    #     }
+    # })
+    #im adding a delay. Displaying the count down with a for loop since powshell doesnt have a built-in countdown.
+    Write-Host "Delaying before sending keys to email"
+    $sleepDuration = 6
+    for ($i = $sleepDuration; $i -ge 0; $i--) {
+        Write-Host "Waiting... $($i)s remaining"
+        Start-Sleep -Seconds 1
+    }
     Write-Host "done delaying"
+
+    #click for the gmail butto
+    $continueWithGmailButton = $driver.FindElementByXPath("//div[text()='Continue with Google']")
+    $continueWithGmailButton.Click()
+
+    Write-Host "button Clicked"
+   # Switch to the newly opened window or frame, also this will act as the delay
+   $driver.SwitchTo().Window($driver.WindowHandles[-1])
+    Write-Host "switched to mini window"
+    Write-Host "done delaying for gmail input"
+
     # Enter the email address
-    # Find the element to insert the email
-    $emailField = $driver.FindElementById("notion-email-input-2")
-    $emailField.SendKeys("cao5224@sdtudents.uc-bcf.edu.ph")
+    $emailField = $driver.FindElementByXPath("//input[@id='identifierId']")
+    $emailField.SendKeys("strawberryloli3@gmail.com")
 
+    # Find the "Next" button by XPath
+    $nextButton = $driver.FindElementByXPath("//button[@type='button']//span[text()='Next']")
 
+    # Click the "Next" button
+    $nextButton.Click()
+
+#     # Find the "Continue with email" button
+#     $continueWithEmailButton = $driver.FindElementByXPath("//div[text()='Continue with email']")
+
+#     # Click the button to continue with email
+#     $continueWithEmailButton.Click()
+#     Write-Host "button Clicked"
+    
 }
 loginToQuickie   
